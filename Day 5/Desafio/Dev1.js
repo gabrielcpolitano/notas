a0ddc85d254ef8204d5ea70d473c4eb036dfed12
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-05-17 22:37:48 -0300

    Obrigado Deus

---
+++ Day 5/Desafio/Dev1.js
@@ -0,0 +1,6 @@
+// Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
+
+let idade = 18;
+let peso = 70;
+
+console.log(idade + " " + peso);
---
+++ Day 5/Desafio/Dev3.js
@@ -0,0 +1,5 @@
+// Declare uma variável chamada "númeroMáximo", atribua a ela o valor máximo que um número pode ter em JavaScript e imprima o valor na console.
+
+númeroMáximo = Number.MAX_VALUE;
+
+console.log(númeroMáximo);
